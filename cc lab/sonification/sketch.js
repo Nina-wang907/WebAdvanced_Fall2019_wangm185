cbf24a5beafd56f36f36539d17562a8ce3ed71cd
Nina-wang907 <53446002+Nina-wang907@users.noreply.github.com>
2019-12-09 14:10:34 -0500

    pm2.5

---
+++ cc lab/sonification/sketch.js
@@ -0,0 +1,93 @@
+let synthstar=['C4','C4','G4','G4','A4','A4','G4','F4','F4','E4','E4','D4','D4','C4','G4','G4','F4','F4','E4','E4','D4','G4','G4','F4','F4','E4','E4','D4','C4','C4','G4','G4','A4','A4','G4','F4','F4','E4','E4','D4','D4','C4'];
+let time=['8n','8n','8n','8n','8n','8n','4n','8n','8n','8n','8n','8n','8n','4n','8n','8n','8n','8n','8n','8n','4n','8n','8n','8n','8n','8n','8n','4n','8n','8n','8n','8n','8n','8n','4n','8n','8n','8n','8n','8n','8n','4n'];
+let index =0;
+
+let synth;
+let loop;
+let cymbal;
+let melody;
+let counter = 0;
+var stars = [];
+let pm25data;
+
+function setup() {
+  createCanvas(600, 600);
+  synth = new Tone. Synth().toMaster();
+  cymbal = new Tone.MetalSynth().toMaster();
+  melody=new Tone.PluckSynth();
+  
+  loop = new Tone. Loop(playSound,'4n');
+  loop.start();
+  Tone.Transport.start();
+  // synth.triggerAttackRelease('C4','2n');
+  for (var i = 0; i < 1000; i++) {
+		stars[i] = new Star();
+  }
+  loadTable('pm2.5.csv', gotData);
+}
+
+function gotData(data){
+  // console.log(data);
+  pm25data = data;
+
+}
+
+function draw() {
+  background(30);
+	
+	for (var i = 0; i < stars.length; i++) {
+		stars[i].draw();
+	}
+}
+
+
+// star class //
+class Star {
+	constructor() {
+		this.x = random(width);
+		this.y = random(height);
+		this.size = random(0.25, 3);
+		this.t = random(TAU);
+	}
+	
+	draw() {
+		this.t += 0.1;
+		var scale = this.size + sin(this.t) * 2;
+		noStroke();
+		ellipse(this.x, this.y, scale, scale);
+	}
+}
+
+function mousePressed() {
+  // console.log(synthstar);
+  // console.log(time);
+  var value = pm25data.rows[0][index][1].arr[5];
+  background(220);
+  console.log(value);
+  index+=1;
+
+  
+  loop.stop();
+
+  let m = map(value,300,405,200,800);
+  
+  synth.triggerAttackRelease('c4','4n');
+  
+}
+
+  
+function playSound(){
+  // if(counter%2==0){
+  synth.triggerAttackRelease(synthstar[index],time[index]);
+  // }
+  
+//   if(counter%4==0){
+//   cymbal.triggerAttackRelease('C4','4n');
+//   }
+
+  
+  // console.log(counter);
+  // counter = (counter+1)%16;
+  index+=1;
+
+}
